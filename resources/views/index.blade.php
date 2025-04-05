c591d9045a29fd402e6b98d53b13ec11ac8af340
Paulo Silva <pauloecomercial367@gmail.com>
2025-04-05 08:43:58 -0300

    :gem: index page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,52 @@
 
 
 @section('content')
+    <main class="py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Contacts</h1>
 
-Layout padrão
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Contact</th>
+                                <th>E-mail</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Nome</td>
+                                <td>988888888</td>
+                                <td>1</td>
+                                <td>
+                                    <button class="btn btn-sm btn-outline-warning">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </button>
+                                    <button class="btn btn-sm btn-outline-danger">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="mt-3">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,6 +10,12 @@
     {{-- BOOTSTRAP --}}
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <script src="/assets/bootstrap/js/jquery-3.7.1.min.js"></script>
+    
+    {{-- ICONS --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- CUSTOM --}}
+    <link rel="stylesheet" href="/assets/css/custom.css">
 </head>
 
 <body>
@@ -19,7 +25,6 @@
 
 
     {{-- BOOTSRAP --}}
-
     <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 
 </body>
