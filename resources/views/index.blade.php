1194c97d737b3c7a06d84db3cb31bdb75aaaf48c
Paulo Silva <pauloecomercial367@gmail.com>
2025-04-05 08:35:41 -0300

    :gear: configuring default aplication blade layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.default')
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('content')
 
-        
-    </head>
-    <body class="antialiased">
-        index view
-    </body>
-</html>
+Layout padrão
+@endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercício Laravel Alfasoft</title>
+
+    {{-- BOOTSTRAP --}}
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <script src="/assets/bootstrap/js/jquery-3.7.1.min.js"></script>
+</head>
+
+<body>
+
+
+    @yield('content')
+
+
+    {{-- BOOTSRAP --}}
+
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
