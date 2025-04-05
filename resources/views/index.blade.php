142621b0546e3c7c97a22af03625d3a54aa51e95
Paulo Silva <pauloecomercial367@gmail.com>
2025-04-05 08:54:23 -0300

    :hammer: using bootstrap card in home layout as default

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,51 +6,56 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="d-flex align-items-center justify-content-between mb-3">
-                        <h1>Contacts</h1>
-                        <a href="{{ route('contacts.create') }}" class="btn btn-sm btn-info">
-                            <i class="fa-regular fa-square-plus"></i> Add new contact
-                        </a>
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center justify-content-between">
+                            <h1>Contacts</h1>
+                            <a href="{{ route('contacts.create') }}" class="btn btn-sm btn-info">
+                                <i class="fa-regular fa-square-plus"></i> Add new contact
+                            </a>
+
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Contact</th>
+                                        <th>E-mail</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Nome</td>
+                                        <td>988888888</td>
+                                        <td>1</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-outline-warning">
+                                                <i class="fa-solid fa-pen-to-square"></i>
+                                            </button>
+                                            <button class="btn btn-sm btn-outline-danger">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="mt-3">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination">
+                                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
                     </div>
 
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Contact</th>
-                                <th>E-mail</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Nome</td>
-                                <td>988888888</td>
-                                <td>1</td>
-                                <td>
-                                    <button class="btn btn-sm btn-outline-warning">
-                                        <i class="fa-solid fa-pen-to-square"></i>
-                                    </button>
-                                    <button class="btn btn-sm btn-outline-danger">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div class="mt-3">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>
-                    </div>
                 </div>
             </div>
         </div>
